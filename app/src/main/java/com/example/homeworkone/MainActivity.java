429bfe28b7076f6a43052ceb567b1e2ad3c4e237
makhortius <dmitriy_makhortov@mail.ru>
2021-05-31 01:36:48 +0300

    FirstStep

---
+++ app/src/main/java/com/example/homeworkone/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.homeworkone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+/*
+        setContentView(R.layout.activity_main);
+*/
+        setContentView(R.layout.activity_second);
+        /*не пойму почему ругается и как это исправить, но на телефоне запускается*/
+    }
+}
